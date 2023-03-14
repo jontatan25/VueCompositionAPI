60655effb01ac0fbdb685d46be8c2a7496339708
Jonathan Sarmiento <jhonn.photo@gmail.com>
2023-03-14 06:08:06 -0500

    feat/added pagination with conditional rendering based on total of pages, EventListView watchEffect implemented

--- real-world-vue/src/router/index.js
+++ real-world-vue/src/router/index.js
@@ -18,18 +18,12 @@ const router = createRouter({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
     },
     {
       path: '/event/:id',
       name: 'event-details',
       props: true,
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: EventDetailsVue
     },
     {
--- real-world-vue/src/views/EventListView.vue
+++ real-world-vue/src/views/EventListView.vue
@@ -1,37 +1,78 @@
+<template>
+  <h1>Events For Good</h1>
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+    <div class="pagination">
+      <RouterLink
+        id="page-prev"
+        :to="{ name: 'event-list', query: { page: page - 1 } }"
+        rel="prev"
+        v-if="page != 1"
+        >&#60; Previous
+      </RouterLink>
+      <RouterLink
+        id="page-next"
+        :to="{ name: 'event-list', query: { page: page + 1 } }"
+        rel="next"
+        v-if="hasNextPage"
+        >Next &#62;
+      </RouterLink>
+    </div>
+  </div>
+</template>
+
 <script setup>
 import EventCard from '../components/EventCard.vue'
 import EventService from '../services/EventService.js'
-import { ref, onMounted } from 'vue'
+import { ref, watchEffect, computed } from 'vue'
 
-  const props = defineProps({
-    page: String,
-  });
+const props = defineProps({
+  page: String
+})
 
 const events = ref(null)
+const totalEvents = ref(0)
 
-onMounted(() => {
+watchEffect(() => {
+  //clearing out events
+  events.value = null
   EventService.getEvents(2, props.page)
     .then((response) => {
       events.value = response.data
+      totalEvents.value = response.headers['x-total-count']
     })
     .catch((error) => {
       console.log(error)
     })
 })
 
+const hasNextPage = computed(() => {
+  const totalPages = Math.ceil(totalEvents.value / 2)
+  return props.page < totalPages
+})
 </script>
-
-<template>
-  <h1>Events For Good</h1>
-  <div class="events">
-    <EventCard v-for="event in events" :key="event.id" :event="event" />
-  </div>
-</template>
 
 <style scoped>
 .events {
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+.pagination {
+  display: flex;
+  width: 290px;
+}
+.pagination a {
+  flex: 1;
+  text-decoration: none;
+  color: #2c3e50;
+}
+
+#page-prev {
+  text-align: left;
+}
+
+#page-next {
+  text-align: right;
 }
 </style>
